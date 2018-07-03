90e12073b2bd23c94fc871c69588a7bdfafa12b2
Gabor <meszaros.gabor.10k@gmail.com>
2018-07-03 10:11:18 +0200

    Update omni.sh

--- omni.sh
+++ omni.sh
@@ -1,13 +1,14 @@
+apt-get update
+apt-get install --assume-yes bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk
+git config --global color.ui true
+git config --global user.name "Gabor Meszaros"
+git config --global user.email "meszaros.gabor.10k@gmail.com"
 wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
 unzip platform-tools-latest-linux.zip -d ~
 mkdir ~/bin
 echo 'if [ -d "$HOME/platform-tools" ] ; then' >> ~/.profile
 echo '    PATH="$HOME/platform-tools:$PATH"' >> ~/.profile
 echo 'fi' >> ~/.profile
-apt-get update
-apt-get install bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk
-git config --global user.name "Gabor Meszaros"
-git config --global user.email "meszaros.gabor.10k@gmail.com"
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 echo 'if [ -d "$HOME/bin" ] ; then' >> ~/.profile
@@ -16,13 +17,12 @@ echo 'fi' >> ~/.profile
 source ~/.profile
 mkdir omni
 cd omni
-repo init -u git://github.com/omnirom/android.git -b android-7.1
+repo init -u git://github.com/omnirom/android.git -b android-8.1
 mkdir -p .repo/local_manifests
-wget https://mesziman.github.io/slim_manifest.xml -O .repo/local_manifests/roomservice.xml
-repo sync
+wget https://mesziman.github.io/omni2.xml -O .repo/local_manifests/roomservice.xml
+repo sync -c -j$( nproc --all ) --force-sync;
 echo "export USE_CCACHE=1" >> ~/.bashrc
 source ~/.bashrc
 source build/envsetup.sh
 wget https://mesziman.github.io/buildoms.sh
 ./prebuilts/misc/linux-x86/ccache/ccache -M 50G
-
