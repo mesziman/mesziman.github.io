a394b47a7dba82c207842ad491d97563ff9cd5d6
Gabor <meszaros.gabor.10k@gmail.com>
2018-08-01 08:27:31 +0200

    Update dus.sh

--- dus.sh
+++ dus.sh
@@ -1,13 +1,14 @@
-apt-get update
-apt-get install bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk
+sudo apt-get update
+sudo  apt-get install --assume-yes  bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk
+git config --global color.ui true
+git config --global user.name "Gabor Meszaros"
+git config --global user.email "meszaros.gabor.10k@gmail.com"
 wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
 unzip platform-tools-latest-linux.zip -d ~
 mkdir ~/bin
 echo 'if [ -d "$HOME/platform-tools" ] ; then' >> ~/.profile
 echo '    PATH="$HOME/platform-tools:$PATH"' >> ~/.profile
 echo 'fi' >> ~/.profile
-git config --global user.name "Gabor Meszaros"
-git config --global user.email "meszaros.gabor.10k@gmail.com"
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 echo 'if [ -d "$HOME/bin" ] ; then' >> ~/.profile
