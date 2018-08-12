25f94cf1890bff89a019d344c38a14efcf04c9db
Gabor <meszaros.gabor.10k@gmail.com>
2018-08-12 14:58:44 +0200

    Update aex.sh

--- aex.sh
+++ aex.sh
@@ -2,6 +2,7 @@ sudo apt-get update
 sudo apt-get install --assume-yes tmux nginx bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk
 wget mesziman.github.io/tmux.conf -O ~/.tmux.conf
 sudo sed -i 's/location \/.*\n/location \/ \{\n autoindex on;/g' /etc/nginx/sites-enabled/default
+sudo rm /var/www/html/index.nginx-debian.html
 sudo systemctl restart nginx
 tmux new -s base
 
