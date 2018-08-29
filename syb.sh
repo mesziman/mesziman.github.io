9b8d6e1353ee092343749c6e8850279593438ea2
Gabor <meszaros.gabor.10k@gmail.com>
2018-08-29 10:28:23 +0200

    Create syb.sh

---
+++ syb.sh
@@ -0,0 +1,37 @@
+sudo apt-get update
+sudo apt-get install --assume-yes tmux nginx bc binutils-aarch64-linux-gnu bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+#sudo apt-get install --assume-yes tmux nginx binutils-aarch64-linux-gnu git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip
+wget mesziman.github.io/tmux.conf -O ~/.tmux.conf
+sudo sed -i 's/location \/.*\n/location \/ \{\n autoindex on;/g' /etc/nginx/sites-enabled/default
+sudo rm /var/www/html/index.nginx-debian.html
+sudo systemctl restart nginx
+tmux new -s base
+
+git config --global color.ui true
+git config --global user.name "Gabor Meszaros"
+git config --global user.email "meszaros.gabor.10k@gmail.com"
+wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+unzip platform-tools-latest-linux.zip -d ~
+mkdir ~/bin
+echo 'if [ -d "$HOME/platform-tools" ] ; then' >> ~/.profile
+echo '    PATH="$HOME/platform-tools:$PATH"' >> ~/.profile
+echo 'fi' >> ~/.profile
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+echo 'if [ -d "$HOME/bin" ] ; then' >> ~/.profile
+echo '    PATH="$HOME/bin:$PATH"' >> ~/.profile
+echo 'fi' >> ~/.profile
+source ~/.profile
+mkdir aexmod
+cd aexmod
+repo init -u https://github.com/syberia-project/manifest.git -b 9.0
+mkdir -p .repo/local_manifests
+wget https://mesziman.github.io/syb.xml -O .repo/local_manifests/roomservice.xml
+repo sync -c -j$( nproc --all ) --force-sync --no-clone-bundle --no-tags;
+echo "export USE_CCACHE=1" >> ~/.bashrc
+source ~/.bashrc
+source build/envsetup.sh
+wget https://mesziman.github.io/buildoms.sh
+./prebuilts/misc/linux-x86/ccache/ccache -M 50G
+source buildoms.sh
+buildaex
