f8bfd57805ae0a1afbc51cb1b77f022438f51d26
Gabor <meszaros.gabor.10k@gmail.com>
2018-09-18 14:31:47 +0200

    Update syb.sh

--- syb.sh
+++ syb.sh
@@ -2,10 +2,9 @@ sudo apt-get update
 sudo apt-get install --assume-yes libomp-dev tmux python nginx bc binutils-aarch64-linux-gnu bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
 #sudo apt-get install --assume-yes tmux nginx binutils-aarch64-linux-gnu git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip
 wget mesziman.github.io/tmux.conf -O ~/.tmux.conf
-sudo sed -i 's/location \/.*\n/location \/ \{\n autoindex on;/g' /etc/nginx/sites-enabled/default
+sudo sed -i 's/location \/.*/location \/ \{\n autoindex on;/g' /etc/nginx/sites-enabled/default
 sudo rm /var/www/html/index.nginx-debian.html
 sudo systemctl restart nginx
-tmux new -s base
 
 git config --global color.ui true
 git config --global user.name "Gabor Meszaros"
