0020d05c966a8e6d3f915874bd4861aa95119285
Gabor <meszaros.gabor.10k@gmail.com>
2020-02-06 12:59:13 +0100

    Create xx.sh

---
+++ xx.sh
@@ -0,0 +1,21 @@
+
+
+git clone https://github.com/mesziman/kernel_xiaomi_sm8150-2 k ;
+cd k ; 
+git config --global color.ui true ;
+git config --global user.name "Gabor Meszaros" ;
+git config --global user.email "meszaros.gabor.10k@gmail.com" ;
+cd .. ;
+
+git clone https://github.com/mesziman/device_xiaomi_cepheus-3 d ;
+cd d ;
+git config --global color.ui true ;
+git config --global user.name "Gabor Meszaros" ;
+git config --global user.email "meszaros.gabor.10k@gmail.com" ;
+cd .. ;
+git clone https://github.com/mesziman/device_xiaomi_sm8150-common-1 dc ;
+cd dc ;
+git config --global color.ui true ;
+git config --global user.name "Gabor Meszaros" ;
+git config --global user.email "meszaros.gabor.10k@gmail.com" ;
+cd .. ;
