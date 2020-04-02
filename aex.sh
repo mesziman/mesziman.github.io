80c59c0c3d079dbbc54aea12d556eb4c61005c50
Gabor <meszaros.gabor.10k@gmail.com>
2020-04-02 15:35:31 +0200

    Update aex.sh

--- aex.sh
+++ aex.sh
@@ -34,7 +34,7 @@ git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(ye
 mkdir meme
 cd meme ; git clone https://github.com/XiaomiFirmwareUpdater/xiaomi-flashable-firmware-creator.py memefirm
 git clone https://github.com/LineageOS/android_vendor_lineage vendor/lineage;
-git clone https://github.com/mesziman/proprietary_vendor_xiaomi-4 vendor/xiaomi;
+git clone --depth 1 --single-branch -b new20 https://github.com/mesziman/proprietary_vendor_xiaomi-4 vendor/xiaomi/cepheus;
 git clone https://github.com/LineageOS/android_prebuilts_tools-lineage prebuilts/tools-lineage;
 git clone --depth 1 --single-branch https://github.com/AndroidDumps/xiaomi_cepheus_dump/ -b cepheus-user-10-QKQ1.190825.002-V11.0.8.0.QFAEUXM-release-keys miui
 cd /root/;
