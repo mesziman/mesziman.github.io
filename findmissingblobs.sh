5bf5f5c14acf053131adda2174335e37d515822c
Gabor <meszaros.gabor.10k@gmail.com>
2020-04-22 14:52:04 +0200

    Update findmissingblobs.sh

--- findmissingblobs.sh
+++ findmissingblobs.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+EXCLUDE_REGEX="^(*.acdb|*.apk|*.b00|*.b01|*.b02|*.b03|*.b04|*.bin|*.cfg|*.cng|*.conf|*.config|*.dat|*.db|*.elf|*.fw|*.jar|*.json|*.mdt|*.mk|*.pem|*.qti|*.qwsp|*.rc|*.so|*.sql|*.tflite|*.txt|*.widevine|*.wmfw|*.xml)"
+if [[ $1 =~ $EXCLUDE_REGEX ]]
+then
+  exit
+fi
 
 findwhat=$(readelf -d $1 | grep "\(NEEDED\)" | sed -r "s/.*\[(.*)\]/\1/")
 for line in $findwhat
