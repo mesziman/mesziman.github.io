9630aaedc594482615726910bcfe8abd59776d58
Gabor <meszaros.gabor.10k@gmail.com>
2020-07-08 12:49:06 +0200

    Update video.sh

--- droid/video.sh
+++ droid/video.sh
@@ -1,12 +1,18 @@
   
 sudo apt-get update
-sudo apt-get install --assume-yes ffmpeg transmission-cli midori xbase-clients tmux
+sudo apt-get install --assume-yes ffmpeg transmission-cli midori xbase-clients tmux handbrake-cli handbrake
 
 wget mesziman.github.io/droid/tmux.conf -O ~/.tmux.conf
 echo "
 function convertmp5() {
 ffmpeg -i $1 -c:v libx265 -preset medium -crf 23 -c:a libopus -b:a 96K "${f%.*}.mkv"
 }
+
+function handbrakeconvertmp5() {
+HandBrakeCLI -i $1 -o x265_"$1" -e x265 -q 22 -E opus -B 96
+}
+
+
 " >> ~/.bashrc
 
 reboot
