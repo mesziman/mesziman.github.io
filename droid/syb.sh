2023bc3cf04034ea758710f89548c332af53f8ec
Gabor <meszaros.gabor.10k@gmail.com>
2020-09-15 15:47:22 +0200

    Update syb.sh

--- droid/syb.sh
+++ droid/syb.sh
@@ -24,7 +24,7 @@ cd meme ; git clone https://github.com/XiaomiFirmwareUpdater/xiaomi-flashable-fi
 git clone https://github.com/LineageOS/android_vendor_lineage vendor/lineage;
 git clone https://github.com/mesziman/proprietary_vendor_xiaomi-4 -b ten vendor/xiaomi;
 git clone https://github.com/LineageOS/android_prebuilts_tools-lineage prebuilts/tools-lineage;
-git clone -b cepheus_eea_12 https://github.com/mesziman/xiaomi_cepheus_dump/  miui
+mkdir miui;cd miui ; wget  https://bigota.d.miui.com/V12.0.2.0.QFAEUXM/miui_CEPHEUSEEAGlobal_V12.0.2.0.QFAEUXM_44dcb66933_10.0.zip
 cd /root/;
 mkdir syb
 cd syb
