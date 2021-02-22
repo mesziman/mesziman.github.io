7e65003c22cc54b66f27a0d8624f710c08fdaca1
Gabor <meszaros.gabor.10k@gmail.com>
2021-02-22 15:51:49 +0100

    Update syb.sh

--- droid/syb.sh
+++ droid/syb.sh
@@ -21,13 +21,14 @@ echo 'if [ -d "$HOME/bin" ] ; then' >> ~/.profile
 echo '    PATH="$HOME/bin:$PATH"' >> ~/.profile
 echo 'fi' >> ~/.profile
 source ~/.profile
-#mkdir meme
-#  mkdir miui;cd miui ; 
-#  wget https://bigota.d.miui.com/21.2.3/miui_CEPHEUS_21.2.3_60881427b7_11.0.zip; cd ..
-#  git clone https://github.com/LineageOS/android_prebuilts_extract-tools prebuilts/extract-tools
-#  git clone https://github.com/LineageOS/android_prebuilts_tools-lineage prebuilts/tools-lineage
-#  git clone https://github.com/LineageOS/android_tools_extract-utils tools/extract-utils
-#  git --depth=1 clone https://github.com/LineageOS/android_vendor_lineage vendor/lineage
+mkdir meme
+cd meme;
+  mkdir miui;cd miui ; 
+  wget https://bigota.d.miui.com/21.2.3/miui_CEPHEUS_21.2.3_60881427b7_11.0.zip; cd ..
+  git clone https://github.com/LineageOS/android_prebuilts_extract-tools prebuilts/extract-tools
+  git clone https://github.com/LineageOS/android_prebuilts_tools-lineage prebuilts/tools-lineage
+  git clone https://github.com/LineageOS/android_tools_extract-utils tools/extract-utils
+  git --depth=1 clone https://github.com/LineageOS/android_vendor_lineage vendor/lineage
 cd /root/;
 mkdir syb
 cd syb
