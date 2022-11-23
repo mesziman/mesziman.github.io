4c203bcd8ce1c31ef806efc3c54ff0ea8e6498c5
Gabor <meszaros.gabor.10k@gmail.com>
2022-11-23 07:41:32 +0100

    Update topaz.sh

--- droid/topaz.sh
+++ droid/topaz.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 sudo apt-get install --assume-yes git-lfs vim unzip brotli libomp-dev ccache tmux libncurses5 python-is-python3 nginx bc binutils-arm-linux-gnueabi binutils-aarch64-linux-gnu bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+#sudo apt-get install --assume-yes git-lfs vim nginx tmux python-is-python3 git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig
 wget https://github.com/gpakosz/.tmux/raw/master/.tmux.conf -O ~/.tmux.conf
 sudo sed -i 's/location \/.*/location \/ \{\n autoindex on;/g' /etc/nginx/sites-enabled/default
 sudo rm /var/www/html/index.nginx-debian.html
